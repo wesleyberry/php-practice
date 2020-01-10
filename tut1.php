77b1368f801be070abb07e9dc109385023fd6f6f
Wesley Berry <wesleyberry52@gmail.com>
2020-01-10 12:31:57 -0600

    defined some variables

---
+++ tut1.php
@@ -0,0 +1,23 @@
+<?php
+$f_name = "Wesley";
+$l_name = "Berry";
+$age = 25;
+$height = 6;
+$can_vote = true;
+$adress = array('street' => '123 Main St', 'city' => 'San Marcos');
+$state = NULL;
+define('PI', 3.14.15);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
