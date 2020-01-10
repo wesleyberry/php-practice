90495b0b8cedb666c17a98d981ac287186ca63b9
Wesley Berry <wesleyberry52@gmail.com>
2020-01-10 13:48:26 -0600

    ternary operators

--- conditionals.php
+++ conditionals.php
@@ -32,6 +32,16 @@
             echo "Here is your Water<br>";
             break;
     }
+
+    $age = 18;
+    $can_vote = ($age >= 18) ? "Can Vote" : "Can't Vote";
+    echo "Vote? : $can_vote<br>";
+
+    if("10" == 10) {
+        echo "They are Equal<br>";
+    } else {
+        echo "They aren't equal<br>";
+    }
     ?>
 </body>
 </html>
