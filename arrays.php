77d63304c799b0dcc19de758ffb6309df627d324
Wesley Berry <wesleyberry52@gmail.com>
2020-01-13 12:55:06 -0600

    loops and sorts in php

--- arrays.php
+++ arrays.php
@@ -8,7 +8,21 @@
 </head>
 <body>
     <?php
-    
+    $friends = array('Joy', 'Willow', 'Ivy');
+    echo 'Wife: ' . $friends[0] . '<br>';
+    $friends[3] = 'Steve';
+    foreach($friends as $f) {
+        printf("Friend : %s<br>", $f);
+    }
+    $me_info = array('Name'=> 'Wesley', 'Street'=> '123 Main');
+    foreach($me_info as $k => $v) {
+        printf("%s : %s<br>", $k, $v);
+    }
+    $friends2 = array('Doug');
+    $friends = $friends + $friends2;
+    sort($friends);
+    rsort($friends);
+    asort($friends);
     ?>
 </body>
 </html>
